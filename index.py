e93343eb852293d94fd8b0b3beaec5599c550f0b
SamMalcolm <sam_malcolm@live.com.au>
2020-01-28 16:59:03 +1100

    Initial commit

---
+++ index.py
@@ -0,0 +1,14 @@
+# Fetches data from TMDb and assigns it to the meta data of video files to be used with iTunes
+
+import requests
+import mutagen
+
+def findShow(name):
+	print(name)
+
+def main():
+	findShow("Seinfeld")
+	return
+
+if __name__ == "__main__":
+	main()
